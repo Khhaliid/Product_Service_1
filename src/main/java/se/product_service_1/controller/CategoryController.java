0c7e9b74aa6774e8a868beb1b0561b9943f0e840
Alexander <97alegre@gafe.molndal.se>
2025-09-03 11:20:13 +0200

    Add descriptions to endpoints

--- src/main/java/se/product_service_1/controller/CategoryController.java
+++ src/main/java/se/product_service_1/controller/CategoryController.java
@@ -1,5 +1,6 @@
 package se.product_service_1.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,20 +18,20 @@ import java.util.List;
 public class CategoryController {
 
     private CategoryService categoryService;
-
+    @Operation(summary = "Get all categories", description = "Returns all categories")
     @GetMapping
     public ResponseEntity<List<Category>> getAllCategories() {
         List<Category> categories = categoryService.getAllCategories();
         return ResponseEntity.ok(categories);
     }
-
+    @Operation(summary = "Get category by name", description = "Search for category by name")
     @GetMapping("/name/{name}")
     public ResponseEntity<Category> getCategoryByName(@PathVariable String name) {
         Category category = categoryService.getCategoryByName(name);
         return ResponseEntity.ok(category);
     }
 
-
+    @Operation(summary = "Add new catagory", description = "Create a new category")
     @PostMapping
     public ResponseEntity<CategoryResponse> addCategory(@RequestBody CategoryRequest categoryRequest) {
         Category category = Category.builder()
@@ -42,7 +43,7 @@ public class CategoryController {
                 .build();
         return ResponseEntity.ok(categoryResponse);
     }
-
+    @Operation(summary = "Delete category", description = "Delete category by name")
     @DeleteMapping("/name/{name}")
     public ResponseEntity<Category> deleteCategoryByName(@PathVariable String name) {
         categoryService.deleteCategoryByName(name);
